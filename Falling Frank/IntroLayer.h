88621b4485b5cc4bb37a3c883bee765599b3880e
jfox <atljeremy@me.com>
2013-09-03 21:33:04 -0400

    Initial Commit

---
+++ Falling Frank/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Falling Frank
+//
+//  Created by Jeremy Fox on 9/3/13.
+//  Copyright jeremyfox 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Falling Frank/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Falling Frank
+//
+//  Created by Jeremy Fox on 9/3/13.
+//  Copyright jeremyfox 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
